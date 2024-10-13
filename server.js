984bd713a66752f5e5381e2a90b99fce8c9ca4f1
parinrava <pravanbakhsh@my.bcit.ca>
2024-10-13 00:50:25 -0700

    something

--- server.js
+++ server.js
@@ -131,5 +131,7 @@ const server = http.createServer((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`); //portnumber
 });
+
+
